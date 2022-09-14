5f9894438d64924cf4e7365f2e0653f1844e7671
tontitor <ailtonarrudatec@gmail.com>
2022-09-14 15:42:20 -0300

    Create component heading.

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import { View, Image } from 'react-native';
 
 import { styles } from './styles';
+import { Heading } from '../../components/Heading';
 
 import logo from '../../assets/logo-nlw-esports.png';
+
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image source={logo} style={styles.logo}/>
+
+      <Heading 
+        title='Encontre seu duo!'
+        subtitle='Selecione o game que deseja jogar...'
+      />
     </View>
   );
 }
