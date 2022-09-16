a9105a410c6ed5d3d54d9231b8e766fbf5dfe037
tontitor <ailtonarrudatec@gmail.com>
2022-09-16 15:41:04 -0300

    Creating routes.

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,11 +4,28 @@ const app = express();
 
 //localhost:3333/ads
 
-app.get('/ads', (request, response) => {
+app.get('/games', (resquest, response) => {
+    return response.json([]);
+});
+
+app.post('/ads', (request, response) => {
+    return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+    //const gameId = request.params.id;
+
+
     return response.json([
         {id: 1, name: 'anuncio 1'},
         {id: 1, name: 'anuncio 1'},
     ])
 });
+
+app.get('/ads/:id/discord', (request, response) => {
+    return response.json([]);
+});
+
+
 
 app.listen(3333);
